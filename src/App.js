f92ef1b360b95a9a6a5993c3ec0b4b8c18307533
waltervivar1133 <waltervivar1133@gmail.com>
2021-10-27 11:56:31 -0500

    Subiendo proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import TopMenu from "./components/TopMenu";
+import Products from "./components/Products";
+import useFetch from "./hooks/useFetch";
+import { urlApiProducts } from "./utils/constants";
+import { STORAGE_PRODUCTS_CART } from "./utils/constants";
 
 function App() {
+  const products = useFetch(urlApiProducts, null);
+  const [productsCart, setProductsCart] = useState([]);
+
+  useEffect(() => {
+    getProductsCart();
+  }, []);
+
+  const getProductsCart = () => {
+    const idsProducts = localStorage.getItem(STORAGE_PRODUCTS_CART);
+
+    if (idsProducts) {
+      const idsProductsSplit = idsProducts.split(",");
+      setProductsCart(idsProductsSplit);
+    } else {
+      setProductsCart([]);
+    }
+  };
+
+  const addProductCart = (id, name) => {
+    const idsProducts = productsCart;
+    idsProducts.push(id);
+    setProductsCart(idsProducts);
+    localStorage.setItem(STORAGE_PRODUCTS_CART, productsCart);
+    getProductsCart();
+    toast.success(`${name} añadido al carrito correctamente.`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TopMenu
+        productsCart={productsCart}
+        getProductsCart={getProductsCart}
+        products={products}
+      />
+      <Products products={products} addProductCart={addProductCart} />
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnVisibilityChange={false}
+        draggable
+        pauseOnHover={false}
+      />
     </div>
   );
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const urlApiProducts = "https://616526d109a29d0017c88f86.mockapi.io/helados";
+export const STORAGE_PRODUCTS_CART = "productsCart";
